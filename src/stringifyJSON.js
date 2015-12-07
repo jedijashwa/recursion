c1a03783a9a21cb07fd54660a8751cf2db31434c
Josh Riesenbach <josh.riesenbach@gmail.com>
2015-12-06 17:42:32 -0700

    Finished stringifyJSON.js. Passes all tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -18,8 +18,9 @@ var stringifyJSON = function(obj) {
   	for (var index = 1; index < obj.length; index++){
   		console.log('17: ' + obj[index]);
   		console.log('18: ' + stringifyJSON(obj[index]));
-  		json += ', ' + stringifyJSON(obj[index]);
+  		json += ',' + stringifyJSON(obj[index]);
   	}
+  	json += ']';
   	// checks for null value and adds null to json as string
   } else if (obj === null) {
   	json += 'null';
@@ -29,22 +30,29 @@ var stringifyJSON = function(obj) {
   	// checks for number or bool and adds number or bool to json
   } else if (typeof obj == 'number' || typeof obj == 'boolean') {
   	json += obj;
-  	// checks for undefined and adds undefined to json as string
-  } else if (typeof obj == 'undefined') {
-  	json += 'undefined';
+  	// checks for undefined or function and ignores
+  } else if (obj == undefined || typeof obj == 'function') {
   	// everything else should be an object and will be treates as such
   } else {
   	// adds starting bracket to json for object
   	json += '{';
+  	// variable that indicates that object is empty
+  	var empty = true;
   	// loops for all elements in object
   	for (var prop in obj){
-  		// writes property name to json in double quotes followed by collon
-  		json += '\"' + prop + '\": ';
-  		// adds stringified version of property to json
-  		json += stringifyJSON(obj[prop]) + ', ';
+  		// only runs if property isn't function or undefined unless it's null
+  		if ((obj[prop] != undefined && typeof obj[prop] != 'function') || obj[prop] === null){
+  			// writes property name to json in double quotes followed by collon
+  			json += '\"' + prop + '\":';
+  			// adds stringified version of property to json
+  			json += stringifyJSON(obj[prop]) + ',';
+  			empty = false;
+  		};
   	};
-  	// removes last comma and space from json and adds closing bracket
-  	json = json.slice(0, json.length - 2) + '}';
+  	// removes last comma and space from json if not an empty object;
+  	if (!empty) json = json.slice(0, json.length - 1);
+  	// adds closing bracket for object to json
+  	json += '}';
   };
   console.log(json);
   return json;
