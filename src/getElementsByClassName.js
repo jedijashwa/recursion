e8d5356244fd629de55d170627b1d1b030772fe4
Josh Riesenbach <josh.riesenbach@gmail.com>
2015-12-09 21:34:37 -0700

    Changed to use childNodes instead of children and classList instead of className.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,21 +8,23 @@ var getElementsByClassName = function(className){
 	// Array to hold all elements found with className
 	var elements =[];
 	// Finds elements in HTML node and all of its children that are elements (nodeType 1)
-	var findElementsWithClass = function (parent, className){
+	var findElementsWithClass = function (parent){
 		// Checks to see if parent has className at all in it's classes
 		if (parent.classList.contains(className)) {
 			// Adds parent to elements array
 			elements.push(parent);
 		};
 		// Loops through all children nodes that are elements
-		for (var i = 0; i < parent.children.length; i++) {
+		for (var i = 0; i < parent.childNodes.length; i++) {
 			// Runs findElementsWithClass on each child node of parent
-			findElementsWithClass(parent.children[i], className);
+			if (parent.childNodes[i].nodeType == 1){
+				findElementsWithClass(parent.childNodes[i]);
+			};
 		};
 		return;
 	};
 	// Runs findElementsWithClass on the body, which recursivly goes through all
 	// it's chiildren as well.
-	findElementsWithClass(document.body, className);
+	findElementsWithClass(document.body);
 	return elements;
 };
